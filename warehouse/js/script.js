d62f739e45c3635eb37c821b6479111702f3a4f7
Hai Nam <haiinamm1408@gmail.com>
2022-12-06 15:34:05 +0700

    responsive

--- warehouse/js/script.js
+++ warehouse/js/script.js
@@ -1,7 +1,7 @@
 
 (function main(){
     fetchCategories()
-    fetchCourses()
+    fetchCourses(4)
     fetchComments()
 })();
 
@@ -35,47 +35,67 @@ function renderStar(star){
     starHtml = '<i class="bi bi-star-fill"></i>'.repeat(star) + '<i class="bi bi-star"></i>'.repeat(5-star)
     return starHtml
 }
+window.onchange = ()=>{
 
-function fetchCourses(){
-    fetch('http://localhost:3000/courses?_limit=4')
-    .then(res => res.json())
-    .then(data => renderCourses(data))
 }
 
-function renderCourses(data){
+let showAllBtn = document.querySelector('.show-all')
+showAllBtn.addEventListener('click', () =>{
+    fetchCourses(100)
+    showAllBtn.classList.add('d-none')
+    hideAwayBtn.classList.remove('d-none')
+})
+
+let hideAwayBtn = document.querySelector('.hide-away')
+hideAwayBtn.addEventListener('click', () =>{
+    fetchCourses(4)
+    hideAwayBtn.classList.add('d-none')
+    showAllBtn.classList.remove('d-none')
+})
+
+
+function fetchCourses(numCourse){
+    fetch('http://localhost:3000/courses')
+    .then(res => res.json())
+    .then(data => renderCourses(data, numCourse))
+}
+
+function renderCourses(data, numCourse){
     const courses = document.querySelector('.courses')
     htmls = ''
-    data.forEach(item => {
-        htmls += `  <li class="row">
-                        <img
-                            src="${item.thumb}">
-
-                        <div class="featured_li__right">
-                            <div class="featured_li__right_top">
-                                <div class="featured_li__star row">
-                                    ${renderStar(item.star)}
+    data.forEach((item, index) => {
+        if((index + 1) <= numCourse){
+            htmls += `  <li class="row">
+                            <img
+                                src="${item.thumb}">
+    
+                            <div class="featured_li__right">
+                                <div class="featured_li__right_top">
+                                    <div class="featured_li__star row">
+                                        ${renderStar(item.star)}
+                                    </div>
+                                    <h3>${item.title}</h3>
+                                    <div class="featured_li__lg row">
+                                        <p>${item.class} Class</p>
+                                        <div class="dot"></div>
+                                        <p>${item.time}</p>
+                                    </div>
+                                    <div class="featured_user row">
+                                        <img class="featured_user__img"
+                                            src="${item.author_thumb}">
+                                        <p>by <span>${item.author_name}</span></p>
+                                    </div>
                                 </div>
-                                <h3>${item.title}</h3>
-                                <div class="featured_li__lg row">
-                                    <p>${item.class} Class</p>
-                                    <div class="dot"></div>
-                                    <p>${item.time}</p>
-                                </div>
-                                <div class="featured_user row">
-                                    <img class="featured_user__img"
-                                        src="${item.author_thumb}">
-                                    <p>by <span>${item.author_name}</span></p>
+                                <div class="featured_li__right_below row">
+                                    <span>$${item.cost}</span>
+                                    <div class="cart row">
+                                        <i class="bi bi-cart3 bg_col"></i>
+                                        <h4>Buy Now</h4>
+                                    </div>
                                 </div>
                             </div>
-                            <div class="featured_li__right_below row">
-                                <span>$${item.cost}</span>
-                                <div class="cart row">
-                                    <i class="bi bi-cart3 bg_col"></i>
-                                    <h4>Buy Now</h4>
-                                </div>
-                            </div>
-                        </div>
-                    </li>`
+                        </li>`
+        }
     });
     courses.innerHTML = htmls
 }
@@ -183,3 +203,87 @@ login_btn.addEventListener('click', () => {
     modal_signIn.classList.add('open_modal_signIn');
     modal_register.classList.remove('open_modal_register');
 });
+
+let modalSignIn = document.querySelector('.modal_signIn')
+let formSignIn = document.querySelector('#form_signIn')
+modalSignIn.addEventListener('click', ()=>{
+    modal_signIn.classList.remove('open_modal_signIn');
+})
+formSignIn.addEventListener('click', e=>{
+    e.stopPropagation()
+})
+
+let modalRegister = document.querySelector('.modal_register')
+let formRegister = document.querySelector('#register_form')
+modalRegister.addEventListener('click', ()=>{
+    modalRegister.classList.remove('open_modal_register');
+})
+formRegister.addEventListener('click', e=>{
+    e.stopPropagation()
+})
+
+//show/hide password
+const inputWrapper = document.querySelectorAll('.form-group.password')
+inputWrapper.forEach(input => {
+    const eyeBtn = input.querySelector('i')
+    const inputPassword = input.querySelector('.input-password')
+
+    eyeBtn.onclick = (e)=>{
+        eyeBtn.classList.toggle('fa-eye')
+        eyeBtn.classList.toggle('fa-eye-slash')
+
+        inputPassword.type = inputPassword.type == 'password' ? 'text':'password'
+    }
+})
+
+//Validator form
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Validator
+    Validator({
+        form: "#form_signIn",
+        formGroupSelector: ".form-group",
+        errorSelector: ".form-message",
+        rules: [Validator.isEmail("#email"), Validator.minLength("#password", 6)],
+        onSubmit: function (data) {
+            // Call API
+            console.log(data);
+        },
+    });
+
+    Validator({
+        form: "#register_form",
+        formGroupSelector: ".form-group",
+        errorSelector: ".form-message",
+        rules: [
+            Validator.isRequired("#fullname", "Vui lòng nhập tên đầy đủ của bạn"),
+            Validator.isEmail("#email"),
+            Validator.minLength("#password", 6),
+            Validator.isRequired("#password_confirmation"),
+            Validator.isConfirmed(
+                "#password_confirmation",
+                function () {
+                    return document.querySelector("#form-1 #password").value;
+                },
+                "Mật khẩu nhập lại không chính xác"
+            ),
+        ],
+        onSubmit: function (data) {
+            // Call API
+            console.log(data);
+        },
+    });
+
+});
+
+
+
+let listBtn = document.querySelector('.list-icon i')
+let headerList = document.querySelector('.header_list')
+let closeHeaderList = document.querySelector('.close_header__list')
+listBtn.onclick = () =>{
+    headerList.style.transform = 'translateX(0)'
+}
+closeHeaderList.onclick = () =>{
+    headerList.style.transform = 'translateX(-100%)'
+}
